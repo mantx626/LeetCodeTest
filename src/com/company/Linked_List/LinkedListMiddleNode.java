0557f2c052d2fba8f7ac2d939c1e2fa7363abb63
MTX <mantx626@gmail.com>
2020-10-16 17:15:24 +0300

    Make LinkNode Public

--- src/com/company/Linked_List/LinkedListMiddleNode.java
+++ src/com/company/Linked_List/LinkedListMiddleNode.java
@@ -1,9 +1,14 @@
 package com.company.Linked_List;
 
 public class LinkedListMiddleNode {
-    public class ListNode{
-        int val;
-        ListNode next;
+    public static class ListNode{
+        public int val;
+        public ListNode next;
+
+
+        public ListNode(int i) {
+
+        }
     }
     public ListNode MiddleNode(ListNode head){
         ListNode i = head, j = head;
